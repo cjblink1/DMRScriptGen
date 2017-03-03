a6b91ad933568f061c636280ee3d41096c821a02
Connor Boyle <cjblink1@gmail.com>
2017-03-03 01:40:41 -0500

    Better UI, no function yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,63 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { SongsListComponent } from './songs-list/songs-list.component';
+import { AddSongComponent } from './add-song/add-song.component';
+import { UploadSongComponent } from './upload-song/upload-song.component';
+import { ShowsListComponent } from './shows-list/shows-list.component';
+import { AddShowComponent } from './add-show/add-show.component';
+
+const appRoutes: Routes = [
+  {
+    path:'songs',
+    component: SongsListComponent
+  },
+  {
+    path:'add-song',
+    component: AddSongComponent
+  },
+  {
+    path:'upload',
+    component: UploadSongComponent
+  },
+  {
+    path:'shows',
+    component: ShowsListComponent
+  },
+  {
+    path:'add-show',
+    component: AddShowComponent
+  },
+  {
+    path:'',
+    redirectTo:'/songs',
+    pathMatch:'full'
+  }
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     SideNavComponent,
-    SongsListComponent
+    SongsListComponent,
+    AddSongComponent,
+    UploadSongComponent,
+    ShowsListComponent,
+    AddShowComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
