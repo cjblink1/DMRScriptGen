6d0f7c40782f4dcf9e06c12ad49b65dfe4ebdd54
Connor Boyle <cjblink1@gmail.com>
2017-03-02 23:46:34 -0500

    More Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { SongsListComponent } from './songs-list/songs-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SideNavComponent,
+    SongsListComponent
   ],
   imports: [
     BrowserModule,
